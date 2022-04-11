19c49309872d5ab057d93c8c63cfe6989391ba65
rakiafelly <rakiafelly14@gmail.com>
2022-04-11 13:43:14 +0100

    exe2 de challenge2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AreaComponent } from './area/area.component';
 import { UserComponent } from './user/user.component';
-
+import { ArticlesComponent } from './articles/articles.component';
+import { TdfComponent } from './tdf/tdf.component';
 const routes: Routes = [
   { path:'area',
   component:AreaComponent,
   },
   { path:'user',
   component:UserComponent,
+  },
+  {
+    path:'articles',
+    component:ArticlesComponent,
+  },
+  {
+    path:'tdf',
+    component:TdfComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AreaComponent } from './area/area.component';
 import { UserComponent } from './user/user.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { TdfComponent } from './tdf/tdf.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AreaComponent,
     UserComponent,
+    ArticlesComponent,
+    TdfComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
