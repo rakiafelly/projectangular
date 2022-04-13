24442666ceb12edd6416b480de627a92b5f10a23
rakiafelly <rakiafelly14@gmail.com>
2022-04-13 23:24:37 +0100

    update

---
+++ src/app/enrollement.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Http2Server } from 'http2';
+@Injectable({
+  providedIn: 'root'
+})
+export class EnrollementService {
+
+  constructor() { }
+}
